c09cc6f359642eb287a7df82865b39106f38efca
rc3chambers <rc3chambers@gmail.com>
2018-05-03 19:55:29 -0400

    add products index and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(name: 'Heels', description: 'for the night out', image_url: '/tmp/heels.jpg', link_url: 'www.google.com', price: 9.99)
+Product.create(name: 'Boots', description: 'for the urban walk', image_url: '/tmp/boots.jpg', link_url: 'www.google.com', price: 9.99)
+Product.create(name: 'Sandals', description: 'for the beach day', image_url: '/tmp/sandals.jpg', link_url: 'www.google.com', price: 9.99)
+Product.create(name: 'Converse', description: 'for the free day', image_url: '/tmp/chucks.jpg', link_url: 'www.google.com', price: 9.99)
