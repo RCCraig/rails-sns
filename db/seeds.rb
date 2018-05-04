82d528f6c4b5502c06da3c0f4a610721311a39dd
rc3chambers <rc3chambers@gmail.com>
2018-05-04 19:55:58 -0400

    added custom css and reseeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,7 @@ Product.create(name: 'Heels', description: 'for the night out', image_url: '/tmp
 Product.create(name: 'Boots', description: 'for the urban walk', image_url: '/tmp/boots.jpg', link_url: 'www.google.com', price: 9.99)
 Product.create(name: 'Sandals', description: 'for the beach day', image_url: '/tmp/sandals.jpg', link_url: 'www.google.com', price: 9.99)
 Product.create(name: 'Converse', description: 'for the free day', image_url: '/tmp/chucks.jpg', link_url: 'www.google.com', price: 9.99)
+Product.create(name: 'Red Heels', description: 'for the night out', image_url: '/tmp/red-heels.jpg', link_url: 'www.google.com', price: 9.99)
+Product.create(name: 'Trainers', description: 'for the workout', image_url: '/tmp/training.jpg', link_url: 'www.google.com', price: 9.99)
+Product.create(name: 'White Heels', description: 'for the night out', image_url: '/tmp/white-shoes.jpg', link_url: 'www.google.com', price: 9.99)
+Product.create(name: 'Running', description: 'for the daily run', image_url: '/tmp/running.jpg', link_url: 'www.google.com', price: 9.99)
